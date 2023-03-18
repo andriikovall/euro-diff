f1885ad935d7448b6678e8b09b42edfae6b32194
andriikovall <anderkoval3@gmail.com>
2023-03-18 18:35:34 +0200

    input refactor, added constraints

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 export const INITIAL_CITY_COINS_COUNT = 1e6;
 
-export const MIN_NUMBER_OF_COUNTRIES = 1;
-export const MAX_NUMBER_OF_COUNTRIES = 20;
-export const MAX_COUNTRY_NAME_LENGTH = 25;
+export const MIN_NUMBER_OF_COUNTRIES = 1; //
+export const MAX_NUMBER_OF_COUNTRIES = 20; // 
+export const MAX_COUNTRY_NAME_LENGTH = 25; //
 
 export const MIN_X_COORDINATE = 1;
 export const MAX_X_COORDINATE = 10;
